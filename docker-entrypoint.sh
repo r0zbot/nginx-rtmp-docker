c3ab8a7fff5216343f4776e25ffe4638300c14cc
r0zbot <r0zbot@users.noreply.github.com>
2019-02-02 21:19:39 -0200

    added SOURCE_URL env variable to docker-entrypoit. oops

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${SOURCE_KEY} ${TRANSCODE_KEY} ${PRESET} ${RESOLUTION} ${BITRATE} ${TRANSCODE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${SOURCE_KEY} ${TRANSCODE_KEY} ${PRESET} ${RESOLUTION} ${BITRATE} ${TRANSCODE_URL} ${SOURCE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
